8ec321ee976459618b57ac1184181174016d26ef
Arilson1 <arilsontere18@hotmail.com>
2024-10-27 20:40:01 -0300

    Adicionando Swagger

--- src/main/java/com/arilson/fitness/fitness_app_api/config/SecurityConfig.java
+++ src/main/java/com/arilson/fitness/fitness_app_api/config/SecurityConfig.java
@@ -30,6 +30,8 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/login").permitAll()
                         .requestMatchers("/api/usuarios").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
